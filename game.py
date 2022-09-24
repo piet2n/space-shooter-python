a25383880975eab25d1f3360ee3afb6f02bc66c9
Jonas Camillus Jeppesen <jonascj@jonascj.dk>
2022-09-24 21:34:49 +0100

    Add spaceship animation

--- game.py
+++ game.py
@@ -12,13 +12,17 @@ screen = pg.display.set_mode((400,600))
 pg.display.set_caption("Space Shooter")
 
 # Spaceship character
-ship_img = pg.image.load("ship.png")
+ship_images = []
+for i in range(3):
+    img = pg.image.load(f"images/ship_{i}.png")
+    ship_images.append(img)
 ship_x = 200 
 ship_y = 500
 
 
 ### Game loop ###
 running = True
+tick = 0
 while running:
 
     ## Event loop  (handle keypresses etc.) ##
@@ -45,10 +49,15 @@ while running:
     ## Drawing ##
     screen.fill((0,0,0)) 
 
-    screen.blit(ship_img, (ship_x, ship_y))
+    # 3 images --> tick % 3
+    # 100% animation speed: tick % 3
+    # 25% animation speed: int(tick/4) % 3
+    r = int(tick/4) % 3 
+    screen.blit(ship_images[r], (ship_x, ship_y))
 
     # Update window with newly drawn pixels
     pg.display.flip()
 
     # Limit/fix frame rate (fps)
     clock.tick(50)
+    tick += 1
